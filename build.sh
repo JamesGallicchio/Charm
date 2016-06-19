5f567635803dc22d6a31b8e342825105935579b0
Gerli Jõgeva <gerlijogeva@gmail.com>
2016-06-19 15:58:20 +0100

    Add a simple build/deploy script
    * compiles all the java files
    * deploys (removing the old version) the app
    * restarts tomcat

---
+++ build.sh
@@ -0,0 +1,34 @@
+if [ ! -f "charm_schema.sql" ]
+then
+    echo "Error: not in Charm directory. Please change directory to repository root"
+    exit 1
+fi
+
+charmDir=`pwd`
+tomcatLibDir="/usr/share/tomcat7/lib"
+webappName="charm"
+tomcatWebappLocation="/var/lib/tomcat7/webapps"
+
+cd "${charmDir}"
+
+## compile jars
+
+echo "== Compiling Java servlets"
+if [ ! -d webapp/WEB-INF/classes ]
+then
+    echo " * Creating directory WEB-INF/classes/"
+    mkdir webapp/WEB-INF/classes
+fi
+
+cd "${charmDir}/webapp/src"
+javac -cp "${tomcatLibDir}/servlet-api.jar" -d "../WEB-INF/classes/" charm/*.java
+
+
+echo "== Deploying webapp (requires sudo)"
+cd "${charmDir}"
+sudo rm -R "${tomcatWebappLocation}/${webappName}"
+sudo cp -R "${charmDir}/webapp" "${tomcatWebappLocation}/${webappName}"
+
+echo "== Restarting tomcat"
+sudo service tomcat7 restart
+
